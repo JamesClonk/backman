37faccacdae5e886fb617f71d489dda2d0a8afb5
JamesClonk <jamesclonk@jamesclonk.ch>
2019-08-04 17:34:31 +0200

    add example listing of existing backups

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/subtle"
 	"fmt"
 	"net/http"
+	"time"
 
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
 	"github.com/labstack/echo/v4"
@@ -35,10 +36,12 @@ func main() {
 	e.DisableHTTP2 = true
 	e.HideBanner = true
 	e.HidePort = true
+	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.Secure())
 	e.Use(middleware.Logger())
-	//e.Use(middleware.Recover())
+	//e.Use(middleware.Recover()) // don't recover, let platform deal with panics
 	e.Use(middleware.Static("/static"))
+
 	// secure whole app with HTTP BasicAuth
 	e.Use(middleware.BasicAuth(func(u, p string, c echo.Context) (bool, error) {
 		if subtle.ConstantTimeCompare([]byte(u), []byte(username)) == 1 && subtle.ConstantTimeCompare([]byte(p), []byte(password)) == 1 {
@@ -86,6 +89,9 @@ func main() {
 	// setup routes
 	e.GET("/", h.hello)
 	e.GET("/health", h.health)
+	e.GET("/backups", h.listBackups)
+	e.GET("/backups/:serviceType", h.listBackups)
+	e.GET("/backups/:serviceType/:serviceName", h.listBackups)
 
 	// start
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", h.App.Port)))
@@ -99,4 +105,88 @@ func (h *Handler) health(c echo.Context) error {
 	return c.JSON(http.StatusOK, struct {
 		Status string `json:"status"`
 	}{"ok"})
+}
+
+func (h *Handler) listBackups(c echo.Context) error {
+	var services []cfenv.Service
+
+	// get list of services to display backups for
+	serviceType := c.Param("serviceType")
+	serviceName := c.Param("serviceName")
+	if len(serviceType) > 0 {
+		if len(serviceName) > 0 {
+			// list backups only for a specific service binding
+			service, err := h.App.Services.WithName(serviceName)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusNotFound, err.Error())
+			}
+			services = append(services, *service)
+		} else {
+			// list backups only for a specific service type
+			var err error
+			services, err = h.App.Services.WithLabel(serviceType)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusNotFound, err.Error())
+			}
+		}
+	} else {
+		// list backups for all services
+		for label, s := range h.App.Services {
+			if isValidServiceType(label) {
+				services = append(services, s...)
+			}
+		}
+	}
+
+	// TODO: collect backups on S3 for services here
+	type File struct {
+		Name      string
+		Size      int64
+		Timestamp time.Time
+	}
+	type Backup struct {
+		ServiceType string
+		ServiceName string
+		Files       []File
+	}
+	backups := make([]Backup, 0)
+	for _, service := range services {
+		backups = append(backups, Backup{
+			ServiceType: service.Label,
+			ServiceName: service.Name,
+			Files: []File{
+				File{
+					Name:      "demo1.txt",
+					Size:      43256,
+					Timestamp: time.Now(),
+				},
+				File{
+					Name:      "demo2.log",
+					Size:      120230,
+					Timestamp: time.Now().AddDate(0, -1, -100),
+				},
+				File{
+					Name:      "db-dump.tar.gz",
+					Size:      68843256,
+					Timestamp: time.Now().AddDate(-1, -1, -1),
+				},
+			},
+		})
+	}
+
+	return c.JSON(http.StatusOK, backups)
+}
+
+func isValidServiceType(serviceType string) bool {
+	switch serviceType {
+	case "postgres":
+		return true
+	case "mariadb":
+		return true
+	case "mysql":
+		return true
+	case "mongodb":
+		return true
+	}
+	return false
 }
