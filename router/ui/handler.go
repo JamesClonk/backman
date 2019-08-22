4717cd923f503b957092d8a610eff3c17e53be19
JamesClonk <jamesclonk@jamesclonk.ch>
2019-08-22 14:24:33 +0200

    use server side rendering

--- router/ui/handler.go
+++ router/ui/handler.go
@@ -4,18 +4,38 @@ import (
 	"net/http"
 
 	echo "github.com/labstack/echo/v4"
+	"gitlab.swisscloud.io/appc-cf-core/appcloud-backman-app/service"
 )
 
 // Handler holds all objects and configurations used across Web-UI requests
-type Handler struct{}
+type Handler struct {
+	Service    *service.Service
+	CFServices map[string][]service.CFService
+}
 
 type Page struct {
-	Title   string
-	Content interface{}
+	Title    string
+	Service  service.CFService
+	Services map[string][]service.CFService
+	Backups  []service.Backup
+	Content  interface{}
 }
 
 func New() *Handler {
-	return &Handler{}
+	s := service.Get()
+
+	services := make(map[string][]service.CFService)
+	for _, s := range s.Services {
+		if _, exists := services[s.Label]; !exists {
+			services[s.Label] = make([]service.CFService, 0)
+		}
+		services[s.Label] = append(services[s.Label], s)
+	}
+
+	return &Handler{
+		Service:    s,
+		CFServices: services,
+	}
 }
 
 func (h *Handler) RegisterRoutes(e *echo.Echo) {
@@ -25,13 +45,27 @@ func (h *Handler) RegisterRoutes(e *echo.Echo) {
 }
 
 func (h *Handler) Index(c echo.Context) error {
-	return c.Render(http.StatusOK, "index.html", Page{Title: "all good!"})
+	return c.Render(http.StatusOK, "index.html", h.newPage("Home"))
 }
 
 func (h *Handler) Services(c echo.Context) error {
-	return c.Render(http.StatusOK, "services.html", Page{Title: "all good!"})
+	return c.Render(http.StatusOK, "services.html", h.newPage("Services"))
 }
 
 func (h *Handler) Backups(c echo.Context) error {
-	return c.Render(http.StatusOK, "backups.html", Page{Title: "all good!"})
+	backups, err := h.Service.GetBackups("", "")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, err.Error())
+	}
+	page := h.newPage("Backups")
+	page.Backups = backups
+
+	return c.Render(http.StatusOK, "backups.html", page)
+}
+
+func (h *Handler) newPage(title string) *Page {
+	return &Page{
+		Title:    title,
+		Services: h.CFServices,
+	}
 }
