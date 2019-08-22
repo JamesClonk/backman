9fe310adfbe81f208bb3b7f240cbb9a7f1add00b
JamesClonk <jamesclonk@jamesclonk.ch>
2019-08-22 12:17:52 +0200

    most complicated css framework ever

--- router/ui/handler.go
+++ router/ui/handler.go
@@ -20,8 +20,18 @@ func New() *Handler {
 
 func (h *Handler) RegisterRoutes(e *echo.Echo) {
 	e.GET("/", h.Index)
+	e.GET("/services", h.Services)
+	e.GET("/backups", h.Backups)
 }
 
 func (h *Handler) Index(c echo.Context) error {
 	return c.Render(http.StatusOK, "index.html", Page{Title: "all good!"})
+}
+
+func (h *Handler) Services(c echo.Context) error {
+	return c.Render(http.StatusOK, "services.html", Page{Title: "all good!"})
+}
+
+func (h *Handler) Backups(c echo.Context) error {
+	return c.Render(http.StatusOK, "backups.html", Page{Title: "all good!"})
 }
